c4bebecec83494de2a23d71ecd26cec95ebc0658
kohbwoo <kohbwoo@gmail.com>
2021-05-23 23:16:18 +0900

    파일 이름 선지정 추가 흑백 변환 기능 개선

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import cv2
 from PIL import Image
 import pytesseract
+
 pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract' #테서렉트 경로 지정
-img = cv2.imread('Sample\Kor\Timetable.png') # 불러올 파일 지정 -> img 에 저장
+
+file = r'Sample\Kor\test2.jpg'#변환한 파일 선택
+
+img = cv2.imread(file) # 불러올 파일 저장
 
 language = int(input("1. English\n2. korean")) #언어 선택 (한+영 혼합 기능 추가필!!)
 if language == 1:
@@ -17,7 +21,7 @@ if prossess == 1:
     for b in boxes.splitlines():
         b = b.split(' ')
         img = cv2.rectangle(img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 1)
-    output = (pytesseract.image_to_string(Image.open('Sample\Kor\Timetable.png'), lang=language)) #이미지 -> 문자열 추출, lang 언어설정
+    output = (pytesseract.image_to_string(Image.open(file), lang=language)) #이미지 -> 문자열 추출, lang 언어설정
     final_str = output[:-1] #문자열 마지막 텍스트 제거
     print(final_str) #출력
     cv2.imshow('img', img) #박스 태깅 된 이미지 띄우기
